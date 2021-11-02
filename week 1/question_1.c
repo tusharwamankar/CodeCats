06a067acdc5333454568c33c25806bcff20853cb
R-a-h-s-u-T <tusharwamankar786@gmail.com>
2021-11-02 20:41:15 +0530

    quick update

---
+++ week 1/question_1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter the no.=\n");
+    scanf("%d", &a);
+    if (a%2==0)
+    printf("even\n");
+    else
+    printf("odd\n");
+    return 0;
+}
